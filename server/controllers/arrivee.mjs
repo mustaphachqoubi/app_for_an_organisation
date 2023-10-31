e1e5205687e4efcf8c09d75f68351145a6ca4ad3
mustapha <mustaphachqoubi88@gmail.com>
2023-10-31 08:33:47 +0000

    i fixed it in the backend as well

--- server/controllers/arrivee.mjs
+++ server/controllers/arrivee.mjs
@@ -10,9 +10,9 @@ export const getArrivee = async (req, res) => {
 
 //post
 export const createArrivee = async (req, res) => {
-    const { ArriveeTh, ArriveeTd } = req.body
+    const { ArriveeTd } = req.body
     try{
-        const arrivee = await Arrivee.create({ ArriveeTh, ArriveeTd })
+        const arrivee = await Arrivee.create({ ArriveeTd })
         res.status(200).json(arrivee)
     } catch (err){
         res.status(400).json({ err: err.message })
